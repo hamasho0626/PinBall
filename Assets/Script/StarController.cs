38eeb500f5e51547130e38c37a1ea8b9981700a8
hamasho0626 <kurono235@gmail.com>
2018-09-16 20:58:47 +0900

    カリキュラム進行中　ステージ作りまで

---
+++ Assets/Script/StarController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarController : MonoBehaviour {
+
+    private float rotSpeed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+        //回転を開始する角度を設定
+        this.transform.Rotate(0, Random.Range(0, 360), 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //毎フレームごとにY軸を中心に1度回転(回転する角度は変数rotSpeedで制御)
+        this.transform.Rotate(0, this.rotSpeed, 0);
+	}
+}
