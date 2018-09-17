8c156d597b0399fa9c7a48956b3c107f817444d4
hamasho0626 <kurono235@gmail.com>
2018-09-17 11:09:26 +0900

    GameOverテキストを追加

---
+++ Assets/Script/BallController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour {
+
+    //追加課題はここに書き込む可能性あり
+
+    //ボールが見える可能性のあるz軸の最大値
+    private float visiblePosz = -7.5f;
+    //ゲームオーバーを表示するテキスト
+    private GameObject gameoverText;
+    
+
+	// Use this for initialization
+	void Start () {
+        this.gameoverText = GameObject.Find("GameOverText");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(this.transform.position.z < this.visiblePosz)
+        {
+            //GameoverTextにゲームオーバーを表示
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+        }
+	}
+}
